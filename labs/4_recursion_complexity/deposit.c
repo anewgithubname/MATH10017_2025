2fa7e02c17a0c42642d844b7ec84b79a07d514c5
Song Liu <song.liu@bristol.ac.uk>
2025-10-06 19:41:29 +0100

    edited code

---
+++ labs/4_recursion_complexity/deposit.c
@@ -0,0 +1,19 @@
+# include <stdio.h>
+
+/*
+new balance = balane + interest
+
+For year y > 0
+balance(y) = balance(y-1) + interest
+
+balance(0) = 1000
+
+*/
+
+double balance(int year){
+    // write your code here:
+}
+
+int main(){
+    printf("%d\n", balance(10));
+}
